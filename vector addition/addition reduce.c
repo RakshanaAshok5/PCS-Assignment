20d9d471843f27c98ce35abcc586beb2a1bca55d
RakshanaAshok5 <114175919+RakshanaAshok5@users.noreply.github.com>
2023-12-01 20:50:42 +0530

    Add files via upload
    This File includes vector addition using
    *Scatter 
    *Gather
    *Broadcast
    *All reduce
    *Reduce

---
+++ vector addition/addition reduce.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <mpi.h>
+
+#define MATRIX_SIZE 4
+
+// Function to generate random values for the matrix
+void generateRandomInput(int matrix[MATRIX_SIZE][MATRIX_SIZE]) {
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            matrix[i][j] = rand() % 10; // Generates random values between 0 and 9
+        }
+    }
+}
+
+// Function for matrix addition
+void matrixAddition(int matrix1[MATRIX_SIZE][MATRIX_SIZE], int matrix2[MATRIX_SIZE][MATRIX_SIZE], int result[MATRIX_SIZE][MATRIX_SIZE]) {
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    int world_size, my_rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    int matrix1[MATRIX_SIZE][MATRIX_SIZE];
+    int matrix2[MATRIX_SIZE][MATRIX_SIZE];
+    int local_result[MATRIX_SIZE][MATRIX_SIZE];
+    int global_result[MATRIX_SIZE][MATRIX_SIZE];
+
+    struct timeval start, end;
+    long long elapsed_time;
+
+    if (my_rank == 0) {
+        generateRandomInput(matrix1); // Generate random input on the root process
+        generateRandomInput(matrix2); // Generate another random matrix
+
+        gettimeofday(&start, NULL); // Start measuring execution time
+    }
+
+    // Broadcast matrices to all processes
+    MPI_Bcast(matrix1, MATRIX_SIZE * MATRIX_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(matrix2, MATRIX_SIZE * MATRIX_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Perform matrix addition locally
+    matrixAddition(matrix1, matrix2, local_result);
+
+    // Sum the local results across all processes using MPI_Reduce
+    MPI_Reduce(local_result, global_result, MATRIX_SIZE * MATRIX_SIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (my_rank == 0) {
+        gettimeofday(&end, NULL); // Stop measuring execution time
+        elapsed_time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+
+        printf("Matrix Addition Result:\n");
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            for (int j = 0; j < MATRIX_SIZE; j++) {
+                printf("%d ", global_result[i][j]); // Print the result
+            }
+            printf("\n");
+        }
+        printf("Elapsed time: %lld microseconds\n", elapsed_time); // Print execution time
+    }
+
+    MPI_Finalize(); // Finalize MPI
+
+    return 0;
+}
